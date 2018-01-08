0c05a26921de9b35c41f8af10ad24008be75d3b2
Sam Berndt <berndtsam@gmail.com>
2018-01-07 20:48:23 -0500

    Added all subsampled files

--- ternary_classifier/classificator_1.5/input/subsampleInput.py
+++ ternary_classifier/classificator_1.5/input/subsampleInput.py
@@ -3,39 +3,45 @@ import math
 start_sample_rate = 1000;
 sample_frequencies = [10, 20, 30, 50, 60, 100, 200, 300, 500, 600, 1000];
 
-writefile = 'Subsamples/s_007_';
+mainFile = 'Subsamples/s_0';
 
 lineIndex = 0;
 
-for sample_frequency in sample_frequencies:
-	sample_rate = math.floor(start_sample_rate / sample_frequency)
-	writefileName = writefile + str(sample_frequency) + '.txt'
+for input_file in range(1,11):
+	if input_file < 10:
+		writeFile = mainFile + '0' + str(input_file) + '_'
+	else:
+		writeFile = mainFile + str(input_file)+ '_'
 
-	with open('s_007.txt', 'r') as eye_record_file:
-		with open(writefileName, 'w') as write_eye_record_file:
-			lineIndex = 0;
-			writeIndex = 0;
-			for line in eye_record_file:
-				# First line
-				if lineIndex == 0:
-					write_eye_record_file.write(line);
-					lineIndex += 1;
-					continue
+	for sample_frequency in sample_frequencies:
+		sample_rate = math.floor(start_sample_rate / sample_frequency)
+		writeFileName = writeFile + str(sample_frequency) + '.txt'
 
-				line = line.split()
-				#print(line)
-				try:
-					if float(line[0]) % sample_rate == 0:
-						line[0] = writeIndex;
-						line[1] = writeIndex;
-						writeIndex += 1;
-						writeLine = '	'
-						for item in line:
-							writeLine += str(item) + '\t'
-						writeLine = writeLine.strip()
-						writeLine += '\n'
-						write_eye_record_file.write(writeLine);
-					lineIndex += 1;
-				except:
-					print(line)
+		with open('s_007.txt', 'r') as eye_record_file:
+			with open(writeFileName, 'w') as write_eye_record_file:
+				lineIndex = 0;
+				writeIndex = 0;
+				for line in eye_record_file:
+					# First line
+					if lineIndex == 0:
+						write_eye_record_file.write(line);
+						lineIndex += 1;
+						continue
+
+					line = line.split()
+					#print(line)
+					try:
+						if float(line[0]) % sample_rate == 0:
+							line[0] = writeIndex;
+							line[1] = writeIndex;
+							writeIndex += 1;
+							writeLine = '	'
+							for item in line:
+								writeLine += str(item) + '\t'
+							writeLine = writeLine.strip()
+							writeLine += '\n'
+							write_eye_record_file.write(writeLine);
+						lineIndex += 1;
+					except:
+						print(line)
 
