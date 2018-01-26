5c7060a337bb825ea1af52766b8fc88abfd03f93
Sam Berndt <berndtsam@gmail.com>
2018-01-25 23:06:05 -0500

    Add generation of charts for each algorithm clean vs noisy on each frequency

---
+++ Charts/I-BDT_Results/ChartScoresOptimalThresholds.m
@@ -0,0 +1,59 @@
+function [ output_args ] = ChartScoresOptimalThresholds( input_args )
+
+[clean, noisy] = ConcatenateData(["s_007", "s_010"], [30, 100, 500, 1000], "Results/FrequencyResultsMu/");
+data = [clean; noisy;];
+
+O_SQnS = 'Opt. SQnS';
+O_FQnS = 'Opt. FQnS';
+O_PQnS = 'Opt. PQnS';
+O_MisFix = 'Opt. MisFix';
+O_FQlS_PQlS = 'Opt. FQlS/PQlS_P/PQlS_V';
+
+SQnS = 'SQnS';
+FQnS = 'FQnS';
+PQnS = 'PQnS';
+MisFix = 'MisFix';
+FQlS = 'FQlS';
+PQlS_P = 'PQlS_P';
+PQlS_V = 'PQlS_V';
+
+Clean = 'Clean ';
+Noisy = 'Noisy ';
+
+optimal_SQnS = 100;
+optimal_FQnS = 83.9;
+optimal_PQnS = 52;
+optimal_MisFix = 7.1;
+optimal_FQlS = 0;
+optimal_PQlS_P = 0;
+optimal_PQlS_V = 0;
+
+x_titles = [30, 100, 500, 1000];
+x_titles = ["Clean " + string(x_titles) "Noisy " + string(x_titles)];
+
+figure;
+bar([1 2 3 4 5 6 7 8],[data(1,:); data(2,:); data(3,:); data(4,:); data(5,:); data(6,:); data(7,:); data(8,:);])
+set(gca,'xticklabel',x_titles)
+refsqns = refline(0, optimal_SQnS);
+reffqns = refline(0, optimal_FQnS);
+refpqns = refline(0, optimal_PQnS);
+refmisfix = refline(0, optimal_MisFix);
+reffqls = refline(0, optimal_FQlS);
+refsqns.Color = 'm';
+reffqns.Color = 'b';
+refpqns.Color = 'c';
+refmisfix.Color = 'g';
+reffqls.Color = 'k';
+
+score_title = "I-BDT Optimal Threshold Results per Frequency";
+y_label = "Behavioral Scores";
+x_label = "Sampling Frequencies";
+
+title(score_title);
+ylabel(y_label);
+xlabel(x_label);
+
+legend(SQnS, FQnS, PQnS, MisFix, FQlS, PQlS_P, PQlS_V, O_SQnS, O_FQnS, O_PQnS, O_MisFix, O_FQlS_PQlS, 'Location', 'bestoutside');
+
+end
+
---
+++ Charts/I-VDT-HMM_Results/ChartScoresOptimalThresholds.m
@@ -0,0 +1,77 @@
+function [  ] = ChartScoresOptimalThresholds( )
+%CHARTSCORESOPTIMALTHRESHOLDS Summary of this function goes here
+%   Detailed explanation goes here
+import_directory = "OptimalResults/";
+mat_file_extension = "-final.mat";
+
+noisey_dataset = "s_010";
+clean_dataset = "s_007";
+
+noisey_filename = import_directory + noisey_dataset + mat_file_extension;
+clean_filename = import_directory + clean_dataset + mat_file_extension;
+
+noisey = load(noisey_filename);
+clean = load(clean_filename);
+
+noisey = noisey.final_threshold_scores;
+clean = clean.final_threshold_scores;
+
+noisey_scores = noisey(:,7:length(noisey));
+clean_scores = clean(:,7:length(clean));
+
+data = [clean_scores; noisey_scores; ];
+
+O_SQnS = 'Opt. SQnS';
+O_FQnS = 'Opt. FQnS';
+O_PQnS = 'Opt. PQnS';
+O_MisFix = 'Opt. MisFix';
+O_FQlS_PQlS = 'Opt. FQlS/PQlS_P/PQlS_V';
+
+SQnS = 'SQnS';
+FQnS = 'FQnS';
+PQnS = 'PQnS';
+MisFix = 'MisFix';
+FQlS = 'FQlS';
+PQlS_P = 'PQlS_P';
+PQlS_V = 'PQlS_V';
+
+Clean = 'Clean ';
+Noisy = 'Noisy ';
+
+optimal_SQnS = 100;
+optimal_FQnS = 83.9;
+optimal_PQnS = 52;
+optimal_MisFix = 7.1;
+optimal_FQlS = 0;
+optimal_PQlS_P = 0;
+optimal_PQlS_V = 0;
+
+x_titles = noisey(:,1);
+x_titles = ["Clean " + string(x_titles); "Noisy " + string(x_titles)];
+
+figure;
+bar([1 2 3 4 5 6 7 8],[data(1,:); data(2,:); data(3,:); data(4,:); data(5,:); data(6,:); data(7,:); data(8,:);])
+set(gca,'xticklabel',x_titles)
+refsqns = refline(0, optimal_SQnS);
+reffqns = refline(0, optimal_FQnS);
+refpqns = refline(0, optimal_PQnS);
+refmisfix = refline(0, optimal_MisFix);
+reffqls = refline(0, optimal_FQlS);
+refsqns.Color = 'm';
+reffqns.Color = 'b';
+refpqns.Color = 'c';
+refmisfix.Color = 'g';
+reffqls.Color = 'k';
+
+score_title = "I-VDT-HMM Optimal Threshold Results per Frequency";
+y_label = "Behavioral Scores";
+x_label = "Sampling Frequencies";
+
+title(score_title);
+ylabel(y_label);
+xlabel(x_label);
+
+legend(SQnS, FQnS, PQnS, MisFix, FQlS, PQlS_P, PQlS_V, O_SQnS, O_FQnS, O_PQnS, O_MisFix, O_FQlS_PQlS, 'Location', 'bestoutside');
+
+end
+
